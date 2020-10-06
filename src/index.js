48f4795c46a819ae4ef4219e0a45c8bbcef252f4
Ярослав Коберник-Березовский <Yaroslavisgood@gmail.com>
2020-10-06 11:43:53 +0300

    feat: solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = [], bracket = {};
+  bracketsConfig.forEach((element, index) => {
+    bracket[bracketsConfig[index][0]] = bracketsConfig[index][1]
+  });
+  for (let i = 0; i < str.split('').length; i++) {
+    if (str.split('')[i] == brackets[brackets.length - 1] || str.split('')[i] == 0) {
+      brackets.pop();
+    } else {
+      brackets.push(bracket[str.split('')[i]]);
+    }
+
+  }
+  if (!brackets.length) return true
+  return false;
+
 }
